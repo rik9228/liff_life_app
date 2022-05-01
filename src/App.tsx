94d33cede5c0bd7742fc3f3ac1f5dd7a1f323539
rik9228 <rank228130@gmail.com>
2022-05-01 16:40:17 +0900

    feature：Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { doc, Timestamp, setDoc } from "firebase/firestore";
+import React from "react";
+import "./App.css";
+import { format as formatTZ } from "date-fns-tz";
+
+import { db } from "./libs/firebase";
 
 function App() {
+  const post = async () => {
+    await setDoc(doc(db, "cities", "LA"), {
+      text: "Los Angeles",
+      createdAt: formatTZ(Timestamp.now().toDate(), "yy/MM/dd HH:mm", {
+        timeZone: "Asia/Tokyo",
+      }),
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={post}>DBテスト</button>
     </div>
   );
 }
---
+++ src/libs/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = getFirestore();
+
+export { firebase, db };
+
+export default firebase;
